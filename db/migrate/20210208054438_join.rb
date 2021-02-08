c4cdac5a41638b276d616dd74fbafc6f48c772b6
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-08 06:04:59 +0000

    Automatically backed up by Learn

--- db/migrate/20210208054438_join.rb
+++ db/migrate/20210208054438_join.rb
@@ -1,6 +1,6 @@
 class Join < ActiveRecord::Migration[5.0]
   def change
-    add_column :rides, :passengers_id, :integer
-    add_column :rides, :taxis_id, :integer
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
   end
 end
